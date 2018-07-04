ca7e6ce4780e4a7ab24783f75a28b42a1a65d2c0
Роман Стёпочкин <r.stepochkin@solarl.ru>
2018-07-04 10:18:53 +0300

    first implementation of the TokenSmartContract and ICSC #SEN-160

---
+++ Senno.SmartContracts.ICSC/InformationContributionSmartContract.cs
@@ -0,0 +1,120 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System.Numerics;
+
+namespace Senno.SmartContracts
+{
+    /// <summary>
+    /// Information contribution smart contract (ICSC).
+    /// This SC will pay reward to Information contributors,
+    /// which gave Senno listeners access to their private 
+    /// channel / group while keeping the data unrevealed
+    /// </summary>
+    public class InformationContributionSmartContract : SmartContract
+    {
+        // payload exchange rate
+        public static int SwapRate() => 1000;
+
+        // TODO set ScriptHash of deployed SennoTokenSmartContract HERE
+        [Appcall("bb3874968979fea4083e70c62a09825fff13d7f5")]
+        public static extern bool Transfer(byte[] from, byte[] to, BigInteger amount);
+
+        public static object Main(string operation, object[] args)
+        {
+            // TODO Stepochkin. Check args array length
+            if (operation == "assign") return Assign((byte[])args[0], (byte[])args[1], (BigInteger)args[2]);
+            if (operation == "rewards") return Rewards((byte[])args[0], (byte[])args[1], (BigInteger)args[2]);
+            return false;
+        }
+
+        private static object Assign(byte[] from, byte[] to, BigInteger payload)
+        {
+            // validating input data
+            if (!ValidateInputData(payload))
+            {
+                return false;
+            }
+
+            // get current payload from storage by address 'to'
+            var currentPayload = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+
+            // calculate total payload
+            currentPayload += payload;
+
+            // calculate and transfer tokens
+            // return the rest of the current payload
+            currentPayload = CalculateAndTransferTokens(from, to, payload, currentPayload);
+
+            // save current payload to storage by address 'to'
+            Storage.Put(Storage.CurrentContext, to, currentPayload);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Pay reward to Information contributors
+        /// </summary>
+        private static object Rewards(byte[] from, byte[] to, BigInteger payload)
+        {
+            // validating input data
+            if (!ValidateInputData(payload))
+            {
+                return false;
+            }
+
+            // get current payload from storage by address 'to'
+            var currentPayload = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+
+            // check that the value does not exceed the current payload
+            if (payload > currentPayload)
+            {
+                return false;
+            }
+
+            // calculate and transfer tokens
+            // return the rest of the current payload
+            currentPayload = CalculateAndTransferTokens(from, to, payload, currentPayload);
+
+            // save current payload to storage by address 'to'
+            Storage.Put(Storage.CurrentContext, to, currentPayload);
+
+            return true;
+        }
+
+        private static bool ValidateInputData(BigInteger payload)
+        {
+            // check that the payload is not passed a zero value
+            if (payload <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static BigInteger CalculateAndTransferTokens(byte[] from, byte[] to, BigInteger payload, BigInteger currentPayload)
+        {
+            // check that addresses 'from' and 'to' do not match
+            if (from != to)
+            {
+                // calculte tokens count by swap rate
+                var tokensCount = currentPayload / SwapRate();
+
+                // if the tokensCount is greater than zero
+                if (tokensCount > 0)
+                {
+                    // call external method for transfer token to address 'to'
+                    if (Transfer(from, to, tokensCount))
+                    {
+                        // if the transfer operation is successful
+                        // calculate the rest of the current payload
+                        currentPayload = currentPayload % SwapRate();
+                    }
+                }
+            }
+
+            return currentPayload;
+        }
+    }
+}
---
+++ Senno.SmartContracts.Token/TokenSmartContract.cs
@@ -0,0 +1,122 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Senno.SmartContracts
+{
+    public class TokenSmartContract : SmartContract
+    {
+        public static readonly byte[] owner = "APgeXL3KRP1kGf4MVPLF2muRUB8U6s5cgz".ToScriptHash();
+
+        // Token Settings
+        public static string Name() => "Senno";
+        public static string Symbol() => "SEN";
+        public static byte Decimals() => 0;
+        private const ulong initSupply = 10000000000;
+
+        public delegate void TransferAction<in T, in T1, in T2>(T p0, T1 p1, T2 p3);
+
+        [DisplayName("transfer")]
+        public static event TransferAction<byte[], byte[], BigInteger> Transferred;
+
+
+        public static object Main(string method, params object[] args)
+        {
+            if (method == "deploy") return Deploy();
+            if (method == "totalSupply") return TotalSupply();
+            if (method == "name") return Name();
+            if (method == "symbol") return Symbol();
+            if (method == "owner") return owner;
+            if (method == "decimals") return Decimals();
+            if (method == "transfer")
+            {
+                if (args.Length != 3) return false;
+                byte[] from = (byte[])args[0];
+                byte[] to = (byte[])args[1];
+                BigInteger value = (BigInteger)args[2];
+                return Transfer(from, to, value);
+            }
+            if (method == "balanceOf")
+            {
+                if (args.Length != 1) return 0;
+                byte[] account = (byte[])args[0];
+                return BalanceOf(account);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Initialization parameters, only once
+        /// </summary>
+        /// <returns></returns>
+        private static bool Deploy()
+        {
+            byte[] total_supply = Storage.Get(Storage.CurrentContext, "totalSupply");
+            if (total_supply.Length != 0) return false;
+
+            Storage.Put(Storage.CurrentContext, owner, initSupply);
+            Storage.Put(Storage.CurrentContext, "totalSupply", initSupply);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Function that is always called when someone wants to transfer tokens.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static bool Transfer(byte[] from, byte[] to, BigInteger value)
+        {
+            if (value <= 0) return false;
+            // Verifies that the calling contract has verified the required script hashes of the transaction/block
+            //if (!Runtime.CheckWitness(from)) return false;
+            // Address only
+            if (to.Length != 20) return false;
+
+            // Get the account value of the source accounts.
+            BigInteger from_value = Storage.Get(Storage.CurrentContext, from).AsBigInteger();
+
+            if (from_value < value) return false;
+
+            if (from == to) return true;
+
+            if (from_value == value)
+                Storage.Delete(Storage.CurrentContext, from);
+            else
+                Storage.Put(Storage.CurrentContext, from, from_value - value);
+
+            // Get the account value of the destination accounts.
+            BigInteger to_value = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+
+            // Set the new account value of the destination accounts.
+            Storage.Put(Storage.CurrentContext, to, to_value + value);
+
+            Transferred(from, to, value);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Get the total token supply
+        /// </summary>
+        /// <returns></returns>
+        public static BigInteger TotalSupply()
+        {
+            return Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+        }
+
+        /// <summary>
+        /// Get the account balance of another account with address
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public static BigInteger BalanceOf(byte[] address)
+        {
+            return Storage.Get(Storage.CurrentContext, address).AsBigInteger();
+        }
+    }
+}
